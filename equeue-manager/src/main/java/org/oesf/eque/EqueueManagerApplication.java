d6d67e754eaa42681c77cf047c4f73a18f1b8ecb
Sergii Krymets <skrymets@intropro.com>
2016-02-12 19:55:35 +0200

    Initial sandbox commit

---
+++ equeue-manager/src/main/java/org/oesf/eque/EqueueManagerApplication.java
@@ -0,0 +1,12 @@
+package org.oesf.eque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqueueManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqueueManagerApplication.class, args);
+    }
+}
